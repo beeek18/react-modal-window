ecffee5e5be4f67c2071bb71c5e3c607f9982430
Bekzodbek <kodirov2000@list.ru>
2023-02-05 00:36:39 +0300

    init

---
+++ src/AppWithProps.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './index.scss';
+
+const Modal = ({ open, setOpen, children }) => (
+  <div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children}
+    </div>
+  </div>
+)
+
+function AppWithProps() {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <div className="App">
+      <button onClick={() => setOpen(true)} className="open-modal-btn">Way of the ninja</button>
+      <Modal open={open} setOpen={setOpen} >
+        <img src="https://media.giphy.com/media/g4N6wTrf1v6yQ/giphy.gif" />
+        <h3>Naruto Uzumaki - 7 Hokage</h3>
+      </Modal >
+    </div>
+  );
+}
+
+export default AppWithProps;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AppAnimated from './AppAnimated';
+import AppWithProps from './AppWithProps';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <AppAnimated /> */}
+    <AppWithProps />
   </React.StrictMode>,
 );
